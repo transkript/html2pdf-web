37dd720247697227d30fea2883127c342924db96
Dezzy <desmondneba134@gmail.com>
2023-09-28 00:03:52 +0100

    (BE #1) Add relaxed dependency and converter router
    Signed-off-by: elroy <elroykdev@gmail.com>

---
+++ public/javascripts/html2pdf.js
@@ -0,0 +1,58 @@
+const { masterToPDF } = require('relaxedjs');
+const puppeteer = require('puppeteer');
+const plugins = require('relaxedjs/src/plugins');
+const path = require('path');
+
+class HTML2PDF {
+    constructor() {
+        this.puppeteerConfig = {
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-translate',
+                '--disable-extensions',
+                '--disable-sync',
+            ],
+        };
+
+        this.relaxedGlobals = {
+            busy: false,
+            config: {},
+            configPlugins: [],
+        };
+
+        this._initializedPlugins = false;
+    }
+
+    async _initializePlugins() {
+        if (this._initializedPlugins) return; // Do not initialize plugins twice
+        for (const [i, plugin] of plugins.builtinDefaultPlugins.entries()) {
+            plugins.builtinDefaultPlugins[i] = await plugin.constructor();
+        }
+        await plugins.updateRegisteredPlugins(this.relaxedGlobals, '/');
+
+        const chrome = await puppeteer.launch(this.puppeteerConfig);
+        this.relaxedGlobals.puppeteerPage = await chrome.newPage();
+        this._initializedPlugins = true;
+    }
+
+    async pdf(templatePath, json_data, tempHtmlPath, outputPdfPath) {
+        await this._initializePlugins();
+        if (this._initializedPlugins) {
+            // Paths must be absolute
+            const defaultTempHtmlPath = tempHtmlPath || path.resolve(__dirname, 'test.html');
+            const defaultOutputPdfPath =
+                outputPdfPath || path.resolve(__dirname, 'outputde.pdf');
+
+            await masterToPDF(
+                templatePath,
+                this.relaxedGlobals,
+                defaultTempHtmlPath,
+                defaultOutputPdfPath,
+                json_data
+            );
+        }
+    }
+}
+
+module.exports = new HTML2PDF();
---
+++ routes/converter.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const path = require("path");
+const fs = require("fs");
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const HTML2PDF = require('../public/javascripts/html2pdf');
+
+router.post('/pdf', async (req, res) => {
+    const file = req.body;
+
+    if (!file) {
+        return res.status(400).send('Missing HTML file');
+    }
+    await converterHandler(res, file)
+});
+
+router.post('/pdf/json', async (req, res) => {
+    const body = req.body;
+
+    if (!body.html) {
+        return res.status(400).send('Missing HTML file');
+    }
+    await converterHandler(res, body.html)
+})
+
+const converterHandler = async (res, htmlContent) => {
+    try {
+        const templatePath = path.resolve(__dirname, `template-${uuidv4()}.html`);
+        const jsonData = {};
+        const tempHTMLPath = path.resolve(__dirname, `temp-${uuidv4()}.html`);
+        const outputPath = path.resolve(__dirname, `output-${uuidv4()}.pdf`);
+
+        fs.writeFileSync(tempHTMLPath, htmlContent);
+        fs.writeFileSync(templatePath, htmlContent);
+
+        await HTML2PDF.pdf(templatePath, jsonData, tempHTMLPath, outputPath);
+
+        res.sendFile(outputPath, () => {
+            console.log("Completed generating PDF.")
+            fs.unlinkSync(outputPath);
+            fs.unlinkSync(templatePath);
+            fs.unlinkSync(tempHTMLPath);
+        });
+    } catch (error) {
+        console.error('Error generating PDF:', error);
+        res.status(500).send('Error generating PDF');
+    }
+}
+
+module.exports = router;
