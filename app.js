6eec925041f8592ca6b3419bae2e708b16c98ba2
elroy <elroykdev@gmail.com>
2023-10-02 17:19:45 +0100

    (BE #3) Add base config for swagger api doc

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const swaggerFile = require('./public/docs/swagger_output.json');
+const swaggerUi = require("swagger-ui-express");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -47,7 +49,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/converter', converterRouter)
+app.use('/api/converter', converterRouter);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
